0cd17699537eea3d585e9197359d09b6d55a4f94
paulohernrique <pereirapaulo436@gmail.com>
2024-09-29 02:53:33 -0300

    Desenvolvimento da lógica do login: criação de usuários + validação dos nomes e campos vazios

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,10 +1,10 @@
 import styles from "./Input.module.css";
 
-function Input({name, type, placeholder, text, value}) {
+function Input({name, type, placeholder, text, value, handleOnChange}) {
     return (
         <div className={styles.input_container}>
             {text && <label htmlFor={name}>{text}</label>}
-            <input name={name} id={name} type={type} placeholder={placeholder} value={value && value}/>
+            <input name={name} id={name} type={type} placeholder={placeholder} value={value && value} onChange={handleOnChange}/>
         </div>
     );
 }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,19 +2,100 @@ import Input from "../form/Input";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./Login.module.css";
 import GoogleIcon from "../../img/Google_Icon.png";
+import { useEffect, useState } from "react";
 
 function Login() {
+    const [user, setUser] = useState({});
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                setUsers(data);
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    function validateUserExistence(user) {
+        return users.some((us) => (us.email === user.email) || (us.phone === user.phone));
+    }
+
+    function validateEmptyField() {
+        const inputs = document.getElementsByTagName("input");
+        const inputsArray = Array.from(inputs);
+        
+        return inputsArray.some((input) => !input.value);  
+    }
+
+    function validateNameLength(name) {
+        return name.length > 50;
+    }
+
+    function formatName(name) {
+        return name.trim().replace(/\s+/g, " ").split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+    }
+
+    function handleChange(e) {
+        setUser({...user, [e.target.name]: e.target.value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (validateEmptyField()) {
+            console.log("campos vazios");
+            return;
+        }
+
+        setUser({...user, "name": formatName(user.name)});
+        
+        if (validateNameLength(user.name) || validateUserExistence(user)) {
+            console.log("usuario ja existe ou nome muito longo");
+            return;
+        }
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                setUsers([...users, data])
+            })
+            .catch(err => console.log(err))
+    }
+
+    function handlePasswordContainer() {
+        setUser({...user, "password": null})
+
+        const password = document.getElementById("password").value;
+        const confirmedPassword = document.getElementById("confirmedPassword").value;
+
+        if (password === confirmedPassword) {
+            setUser({...user, "password": confirmedPassword});
+        }
+    }
+
     return (
         <main className={styles.login}>
             <div className={styles.login_container}>
-                <form className={styles.login_form}>
+                <form className={styles.login_form} onSubmit={handleSubmit}>
                     <h1>Login</h1>
-                    <Input name="name" type="text" placeholder="Insira seu nome" text="Nome"/>
-                    <Input name="email" type="email" placeholder="Insira seu e-mail" text="E-mail"/>
-                    <Input name="phone" type="tel" placeholder="XX 9XXXX-XXXX" text="Telefone"/>
+                    <Input name="name" type="text" placeholder="Insira seu nome" text="Nome" handleOnChange={handleChange}/>
+                    <Input name="email" type="email" placeholder="Insira seu e-mail" text="E-mail" handleOnChange={handleChange}/>
+                    <Input name="phone" type="tel" placeholder="XX 9XXXX-XXXX" text="Telefone" handleOnChange={handleChange}/>
                     <div className={styles.password_container}>
-                        <Input name="password" type="password" placeholder="Insira sua senha" text="Senha"/>
-                        <Input name="repeatedPassword" type="password" placeholder="Confirme sua senha"/>
+                        <Input name="password" type="password" placeholder="Insira sua senha" text="Senha" handleOnChange={handlePasswordContainer}/>
+                        <Input name="confirmedPassword" type="password" placeholder="Confirme sua senha" handleOnChange={handlePasswordContainer}/>
                     </div>
                     <div className={styles.buttons}>
                         <SubmitButton  text="Criar conta"/>
